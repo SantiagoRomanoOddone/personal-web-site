a09d59cce72755045c3c02c22094b901f10c5b08
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2024-12-08 11:38:14 -0300

    adding data

---
+++ scr/info.py
@@ -0,0 +1,113 @@
+info = {
+    'about_me': """
+                Machine Learning Engineer / Data Scientist specializing in the development of data science and artificial intelligence solutions for industry and business.
+                """,
+    'contact': {
+        'data': """
+            <div style="text-align: left">        
+            - Email: santiagoromano15@gmail.com <br>
+            - Phone: +5493884840234 <br>
+            - Location: Buenos Aires, Argentina <br>      
+            </div>
+            """,
+        'style': "<h2 style='text-align: left'>Contact</h2>"           
+    },
+    'profiles': {
+        'data': """
+            <div style="text-align: left"> 
+            - <a href="https://linkedin.com/in/santiagoromanooddone" target="_blank">LinkedIn</a><br>
+            - <a href="https://github.com/SantiagoRomanoOddone" target="_blank">GitHub</a>
+            </div>
+            """,
+        'style': "<h2 style='text-align: left'>Profiles</h2>"         
+    },
+    'skills':{   
+        'title': ":blue[My Core Tools & Technologies] ⚒️" ,
+        'data': ['Python','R','PySpark','.NET Core','SQL','AWS','Github','Gitlab','Pytorch','Scikit-Learn']
+    },
+    'work_experience': {
+        'scanntech': {
+            'title': "Data Scientist",
+            'company': "[Scanntech Uruguay](https://scanntech.com)",
+            'duration': "02/2024 - Present, Buenos Aires, Argentina",
+            'description': """
+                - Development of advanced forecasting models for large retail enterprises.
+                - Development of stockout prediction models.
+                - Development of a software product for smart inventory replenishment.
+            """,
+            'image': 'path_to_scanntech_image'
+        },
+        'accenture': {
+            'title_ml': "Machine Learning Engineer",
+            'company_ml': "[Accenture Argentina](https://www.accenture.com/ar-es)",
+            'duration_ml': "07/2023 - 01/2024, Buenos Aires, Argentina",
+            'description_ml': """
+                - Execution of global human resources demand and supply prediction models, including data preprocessing, model training, and result analysis.
+                - Collaboration with data science teams to analyze monthly results and improve model predictions.
+                - Development of supervised and unsupervised machine learning models for client clustering and cluster prediction.
+            """,
+            'title_app': "Application Developer",
+            'company_app': "[Accenture Argentina](https://www.accenture.com/ar-es)",
+            'duration_app': "02/2022 - 06/2023, Buenos Aires, Argentina",
+            'description_app': """
+                - Development of ETL solutions for importing/exporting, loading, and processing data.
+                - Development of .NET CORE applications.
+            """,
+            'image': 'path_to_accenture_image'
+        }
+    },
+    'education': {
+        'master': {
+            'title': "[Master in Management + Analytics](https://www.utdt.edu/listado_contenidos.php?id_item_menu=25098)",
+            'institution': "[Torcuato Di Tella University](https://www.utdt.edu)",
+            'duration': "03/2023 - Present, Buenos Aires, Argentina"
+        },
+        'bachelor': {
+            'title': "Industrial Engineering",
+            'institution': "[National Technological University](https://www.frc.utn.edu.ar)",
+            'duration': "03/2015 - 12/2020, Córdoba, Argentina",
+            'gpa': "GPA: 8.35/10"
+        }
+    },
+    'footer': {
+        'header': "Get In Touch With Me!",
+        'contact_form': """
+            <form action="https://formsubmit.co/santiagoromano15@gmail.com" method="POST">
+                <input type="hidden" name="_captcha" value="false">
+                <input type="text" name="name" placeholder="Your name" required>
+                <input type="email" name="email" placeholder="Your email" required>
+                <textarea name="message" placeholder="Your message here" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+        """,
+        'credits': """
+            <div style="text-align: center;">
+                <p>Made with Streamlit by Santiago Romano Oddone</p>
+                <p>
+                    <a href="https://linkedin.com/in/santiagoromanooddone" target="_blank">LinkedIn</a> |
+                    <a href="https://github.com/SantiagoRomanoOddone" target="_blank">GitHub</a>
+                </p>
+            </div>
+        """
+    },
+    'projects': [
+        {
+            "title": "Rossmann Store Sales",
+            "description": """
+                Rossmann operates over 3,000 drug stores in 7 European countries.
+                This competition is about predicting 6 weeks of daily sales for 1,115 stores located across Germany. Reliable sales forecasts enable store managers to create effective staff schedules that increase productivity and motivation.
+            """,
+            "image": "images/sales-forcasting.png",
+            "link": "https://github.com/SantiagoRomanoOddone/rossmann-store-sales"
+        },
+        {
+            "title": "Contact Prediction Challenge",
+            "description": """
+                Properatti is a prominent real estate portal across various countries. The challenge involved developing a predictive model to determine if property listings published during specific months in 2022 would receive a minimum of three contacts within the first 15 days of publication.
+            """,
+            "image": "images/properati.png",
+            "link": "https://github.com/SantiagoRomanoOddone/contact-prediction-challenge"
+        },
+        # Add more projects here
+    ]
+}
